beb1ef5809c5618b94f8547470b1e235a8730b2f
xue2sheng <andres.sevillano.vallejo@gmail.com>
2023-04-30 01:28:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # minimal-compose-sample
 Minimal Compose Sample to be used as template for command line or Intellij IDEA
+
+## Generate jar file
+
+```
+gradle packageReleaseUberJarForCurrentOS
+```
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+   kotlin("jvm") version "1.8.20"
+   id("org.jetbrains.compose") version "1.4.0"
+}
+
+
+repositories {
+   mavenCentral()
+   maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+   google()
+}
+
+
+dependencies {
+   implementation(compose.desktop.currentOs)
+}
+
+
+compose.desktop {
+   application {
+       mainClass = "MainKt"
+   }
+}
+
